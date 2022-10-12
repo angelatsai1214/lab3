b277b7b9f86c831e61009fb75bce2aec411fad63
Angela Tsai <chojungtsai1214@gmail.com>
2022-10-12 14:32:19 -0700

    Fixed ReverseInPlace

--- ArrayTests.java
+++ ArrayTests.java
@@ -16,9 +16,37 @@ public class ArrayTests {
   }
 
   @Test
-  public void testReverse4Elem() {
+  public void testReversed4Elem() {
     int[] input1 = { 1, 2, 3, 4 };
     assertArrayEquals(new int[] { 4, 3, 2, 1 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPlace4Elem() {
+    int[] input1 = { 1, 2, 3, 4 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[] { 4, 3, 2, 1 }, input1);
+  }
+
+  @Test
+  public void testAverageWithoutLowest1() {
+    double[] input1 = { 99.1, 92.0, 90.0, 89.0, 88.5 };
+    assertEquals(92.5, ArrayExamples.averageWithoutLowest(input1), 0.1);
+  }
+
+  @Test
+  public void testAverageWithoutLowest2() {
+    double[] input1 = {};
+    assertEquals(0, ArrayExamples.averageWithoutLowest(input1), 0.1);
+
+    double[] input2 = { 1 };
+    assertEquals(0, ArrayExamples.averageWithoutLowest(input2), 0.1);
+  }
+
+  @Test
+  public void testAverageWithoutLowest3() {
+    double[] input1 = { -2, 0, 3, 3 };
+    assertEquals(2, ArrayExamples.averageWithoutLowest(input1), 0.1);
   }
 
 }
